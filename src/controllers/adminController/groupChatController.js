4d23c56d45e964c7d000d3356e212e2ea73809cc
faye wong <trandaosimanh@gmail.com>
2020-07-15 17:44:17 +0700

    cc

--- src/controllers/adminController/groupChatController.js
+++ src/controllers/adminController/groupChatController.js
@@ -1,9 +1,41 @@
+import GroupChat from "../../models/chatGroupModel";
+import Message from "../../models/messageModel";
+import {
+  bufferToBase64,
+  lastItemOfArray,
+  convertTimestampToHumanTime,
+} from "../../helpers/clientHelper";
+
+// @dest admin/groupChats/
 const list = async (req, res, next) => {
-  return res.render("main/admin/chatGroups/list", { url: "groupChat" });
+  const groupChats = await GroupChat.find();
+  return res.render("main/admin/chatGroups/list", {
+    groupChats,
+    url: "groupChat",
+  });
 };
 
+// @dest admin/groupChats/:id
 const retrieve = async (req, res, next) => {
-  return res.render("main/admin/chatGroups/retrieve", { url: "groupChat" });
+  try {
+    const groupChat = await GroupChat.findById(req.params.id);
+    if (!groupChat) {
+      console.log("Group chat not found");
+      return res.redirect("/admin/groupChats");
+    }
+    const messages = await Message.model.find({ receiverId: req.params.id });
+
+    return res.render("main/admin/chatGroups/retrieve", {
+      messages,
+      url: "groupChat",
+      sender: req.query.sender,
+      groupChat,
+      bufferToBase64,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.redirect("/admin/groupChats");
+  }
 };
 
 module.exports = {
