d845b998ebeba0eaa53ce9a82b883bc96d864f60
faye wong <trandaosimanh@gmail.com>
2020-07-22 13:04:44 +0700

    cc

--- src/controllers/adminController/chatController.js
+++ src/controllers/adminController/chatController.js
@@ -24,7 +24,7 @@ const retrieve = async (req, res, next) => {
     messages,
     bufferToBase64,
     convertTimestampToHumanTime,
-    user: req.params.id,
+    sender: req.params.id,
   });
 };
 
